39eb64c8a3c3ef97e994620945a480a8de88a4df
Chris Mumford <mumfordnsons@gmail.com>
2014-08-17 12:16:11 +0100

    Updated router
    If the router is instantiated, set the new ref and call getImages()

--- js/common/router.js
+++ js/common/router.js
@@ -10,7 +10,7 @@ Backbone.$ = $;
 // Set the router as the module exports
 module.exports = Backbone.Router.extend({
 
-	// The app routes ("routeUrl": "routeName")
+    // The app routes ("routeUrl": "routeName")
     routes: {
         "": "home",
         "results/:ref": "results"
@@ -20,12 +20,12 @@ module.exports = Backbone.Router.extend({
     home: function () { 
         utils.log('Home', 'routeChange');
 
-		// Require views.
-		var NavView    = require('../views/nav'),
+        // Require views.
+        var NavView    = require('../views/nav'),
             SearchView = require('../views/search');
 
-		// Create instances of views if they don't exist.
-		if (!this.navView) this.navView = new NavView({ el: $(globals.elNav) });
+        // Create instances of views if they don't exist.
+        if (!this.navView) this.navView = new NavView({ el: $(globals.elNav) });
         if (!this.searchView) this.searchView = new SearchView({ el: $(globals.elSearch) });            
     },
 
@@ -39,11 +39,16 @@ module.exports = Backbone.Router.extend({
             ResultsView = require('../views/results');
 
         // Create instances of views if they don't exists.
-        if (!this.navView) navView = new NavView({ el: $(globals.elNav) });
-        if (!this.searchView) searchView = new SearchView({ el: $(globals.elSearch) });   
-        if (!this.resultsView) resultsView = new ResultsView({ el: $(globals.elResults), sObfuscated: ref }); 
-
-
+        if (!this.navView) this.navView = new NavView({ el: $(globals.elNav) });
+        if (!this.searchView) this.searchView = new SearchView({ el: $(globals.elSearch) });   
+        if (!this.resultsView)  {
+            this.resultsView = new ResultsView({ el: $(globals.elResults), sObfuscated: ref }); 
+        } else {
+            // Set the option
+            this.resultsView.options.sObfuscated = ref;
+            // Call function to get images
+            this.resultsView.getImages();
+        }
         
     }
 
